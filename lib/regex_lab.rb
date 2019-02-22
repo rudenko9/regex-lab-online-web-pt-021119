b7bb3e072b21cbf84aa06e9b093a41901efd77d3
Nataliya R <n.rudenko@gmail.com>
2019-02-22 01:18:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
 
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,6 +8,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+  word.grep(/^\w{5}\s/)
 
 end
 
